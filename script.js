7ab34b0fd25620ac4e52afa387707d43372c12a6
wubbie88 <gallaher.emily@yahoo.com>
2024-08-25 19:32:46 -0500

    added to the user experience with styling and javascript

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ function generatePoem(event) {
 
   let apiKey = "5659f63f4bto410aa8c8d28bce1757b0";
   let prompt = `User instructions are: Generate a French poem about ${instructionsInput.value}`;
-  let context =  "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let context =  "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+ let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
   axios.get(apiUrl).then(displayPoem);
-
-
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
