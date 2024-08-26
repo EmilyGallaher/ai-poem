2439ed4688a3585d139935ae8314a4c52ea1b0b3
wubbie88 <gallaher.emily@yahoo.com>
2024-08-25 19:02:40 -0500

    added AI API to Javascript and formatting for poem

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+function displayPoem(response)  {
+    new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+
+function generatePoem(event) {
+  event.preventDefault();
+    let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "5659f63f4bto410aa8c8d28bce1757b0";
+  let prompt = `User instructions are: Generate a French poem about ${instructionsInput.value}`;
+  let context =  "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
