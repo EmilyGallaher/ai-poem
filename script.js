0d8977b06d46324e2271418919deb5a8b418d3c2
wubbie88 <gallaher.emily@yahoo.com>
2024-08-25 19:38:09 -0500

    changing project from French poems to English poems

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
 
   let apiKey = "5659f63f4bto410aa8c8d28bce1757b0";
-  let prompt = `User instructions are: Generate a French poem about ${instructionsInput.value}`;
+  let prompt = `User instructions are: Generate a poem about ${instructionsInput.value}`;
   let context =  "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
